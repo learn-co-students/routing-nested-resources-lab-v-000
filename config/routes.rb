25907ed6802127e86000cd47df3efbb0e0a4796a
Mitul Mistry <mitul.mistry1@gmail.com>
2016-02-05 22:35:33 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists do
+    resources :songs, only: [:show, :index] #so you can see all songs for an artist(index), and an individual song for an artist(show)
+  end
+  
   resources :songs
 end
