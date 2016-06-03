8dace863e11e12ba92ec2806ffbb4104f5a8d8cd
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-03 13:11:46 -0700

    Done.

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   gem 'pry'
+  gem 'pry-nav'
   gem 'capybara'
   gem 'rspec-rails'
   gem 'shoulda'
@@ -17,4 +18,3 @@ end
 group :development do
   gem 'web-console', '~> 2.0'
 end
-
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,36 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      begin
+        @artist = Artist.find(params[:artist_id])
+        @songs = @artist.songs
+      rescue ActiveRecord::RecordNotFound
+        redirect_to artists_path,
+        :flash => { :alert => "Artist not found" }
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    # binding.pry
+    if params[:artist_id]
+      begin
+        @artist = Artist.find(params[:artist_id])
+      rescue ActiveRecord::RecordNotFound
+        redirect_to artists_path,
+        :flash => { :alert => "Artist not found." }
+      end
+      begin
+        @song = Song.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        redirect_to artist_songs_path(@artist),
+        :flash => { :alert => "Song not found." }
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
@@ -50,4 +76,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
