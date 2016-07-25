b350eb990877526543afb8efb8e48a90aea2195e
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-25 16:03:12 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  resources :artists, only: [:show] do
+    resources :songs, only: [:show, :index]
+  end
+
+  resources :artists, only: [:index]
+  resources :songs, only: [:index, :show, :edit, :new, :create]
+
 end
