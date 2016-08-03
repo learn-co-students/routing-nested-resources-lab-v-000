f9236e0bbe1da0201d2bfc5bf373fbcd7658f3cf
aprietof <adrianprieto00@gmail.com>
2016-08-03 12:57:46 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :artists
+
+  resources :artists do
+    # routes to songs as a nested resource of artist
+    # only routes to index and show for songs
+    resources :songs, only: [:index, :show ]
+  end
+
   resources :songs
 end
