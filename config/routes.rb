41325b6af84939906eaf4223a954c402cf9c62f7
Joe <jsbenning4@gmail.com>
2016-09-01 14:45:24 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+
+  resources :artists do
+    resources :songs, only: [:index, :show]
+  end
+
+  resources :songs do
+    resources :artists, only: [:index, :show]
+  end
+
+
 end
