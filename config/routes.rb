9e8dfa30559797bac6ebfbd379f5eae5efa8c4dd
Clare <clare.e.horton@gmail.com>
2016-11-15 18:20:59 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists, only: [:show] do
+    resources :songs, only: [:index, :show]
+  end
+  
+  resources :artists, only: [:index]
+  
   resources :songs
 end
