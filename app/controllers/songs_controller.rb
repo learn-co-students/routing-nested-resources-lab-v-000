9953fb3bea57f8021e84e64193d9c948d0b13d9c
Jon Fougner <jon.fougner@gmail.com>
2017-02-24 18:52:41 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,31 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      if Artist.find_by(id: params[:artist_id])
+        @artist = Artist.find_by(id: params[:artist_id])
+        # Check solution for a way to avoid hitting the db twice above
+        @songs = @artist.songs
+      else
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      if Artist.find_by(id: params[:artist_id])
+        @artist = Artist.find_by(id: params[:artist_id])
+        if @artist.songs.find_by(id: params[:id])
+          @song = @artist.songs.find_by(id: params[:id])
+        else
+          flash[:alert] = "Song not found."
+          redirect_to artist_songs_path(@artist)
+        end
+      end
+    end
   end
 
   def new
@@ -50,4 +71,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
