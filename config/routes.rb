feeb4d7734909752bbcc5dc2da4ae9496dcb3ce3
Gordon Siu <gs2@GSMB2.local>
2017-02-24 08:54:54 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :artists
+
+  resources :artists, only: [:index, :new, :create, :update, :delete]
+
+  resources :artists, only: [:show] do
+      resources :songs, only: [:show, :index]
+  end
+
   resources :songs
 end
