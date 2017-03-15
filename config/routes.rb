6f0d45ed292a62675b5405d5cd135970bd51cb39
warrend <warrend555@gmail.com>
2017-03-15 18:28:05 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+
+  resources :artists, only: [:show] do 
+  	resources :songs, only: [:index, :show]
+  end
+
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  resources :artists, only: [:index, :new, :create, :edit, :update]
+
+  
+
 end
