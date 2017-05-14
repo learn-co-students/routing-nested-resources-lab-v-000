29381223cf04b584a23cd31eced74d51e1aacf37
cquinones100 <cquinones100@gmail.com>
2017-05-14 11:08:58 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :artists, only: [:show] do
+    resources :songs, only: [:show, :index]
+  end
+
   resources :artists
-  resources :songs
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 end
