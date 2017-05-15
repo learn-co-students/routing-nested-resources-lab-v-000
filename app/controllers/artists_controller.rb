de90c499c6117d4ef12ada26fb2d7e7caeefabba
mxdavis <md01@me.com>
2017-05-16 01:46:58 +0300

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,10 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
+    @artist = Artist.find_by(id: params[:id])
+    if @artist.nil?
+      redirect_to artists_path, alert: "Artist not found"
+    end
   end
 
   def new
