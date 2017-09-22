44fb333e63cb02097702fd52571e0f28179943c8
Andrew <github email address>
2017-09-21 20:03:37 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,7 @@ class SongsController < ApplicationController
         redirect_to artist_songs_path(@artist), alert: "Song not found"
       end
     else
-      @songs = Song.find(params[:id])
+      @song = Song.find(params[:id])
       end
   end
 
