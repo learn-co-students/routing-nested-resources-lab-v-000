c0b81f3cdcaaa31c34ad2d89f195c791b317a916
Jennifer Haddock <jentx123@hotmail.com>
2017-09-24 05:04:49 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists, only: [:show, :index] do
+    resources :songs, only: [:show, :index]
+  end
+  resources :artists, only: [:new, :create, :edit, :update]
   resources :songs
 end
