2ffa31a280ffd11eb5e46c9c8d2cfc7fbc72a8eb
Chase Paules <cpaules@gmail.com>
2017-10-04 20:24:45 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :artists, only: [:show] do
+    # nested resource for posts
+    resources :songs, only: [:show, :index]
+  end
+
   resources :artists
   resources :songs
+
 end
