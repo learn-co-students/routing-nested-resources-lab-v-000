bf3858090f834da379602c13dade5ea185eb7753
Nicholas D'Amico <nickalan82@icloud.com>
2017-10-15 11:36:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists do 
+  	resources :songs, only: [:index, :show]
+  end
+  # this is what we want /artists/1/songs/1
   resources :songs
 end
