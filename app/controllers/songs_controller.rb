0ce644237183a591598c37e7a62d46d4a9ef1760
marielfrank <marielbeth@gmail.com>
2017-10-19 11:31:42 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,29 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id] && find_artist
+      @songs = find_artist.songs
+    elsif params[:artist_id]
+      flash[:alert] = "Artist not found"
+      redirect_to artists_path
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id] && find_artist && find_song
+      @artist = find_artist
+      @song = find_song
+    elsif params[:artist_id] && find_artist
+      @artist = find_artist
+      flash[:alert] = "Song not found"
+      redirect_to artist_songs_path(@artist.id)
+    elsif find_song
+      @song = find_song
+    else
+      flash[:alert] = "Song not found"
+      redirect_to artists_path
+    end
   end
 
   def new
@@ -22,11 +41,11 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    @song = find_song
   end
 
   def update
-    @song = Song.find(params[:id])
+    @song = find_song
 
     @song.update(song_params)
 
@@ -38,7 +57,7 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
+    @song = find_song
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
@@ -48,6 +67,14 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
+  end
+
+  def find_song
+    Song.find_by(id: params[:id])
+  end
+
+  def find_artist
+    Artist.find_by(id: params[:artist_id])
   end
 end
 
