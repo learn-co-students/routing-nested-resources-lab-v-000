0cc3bc1d381d1ea085b6e888eac17a6c68ab1a11
Joanna <jmgsese@gmail.com>
2017-11-06 14:26:35 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,6 +15,7 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
+      binding.pry
       @artist = Artist.find_by(id: params[:artist_id])
       @song = @artist.songs.find_by(id: params[:id])
       if @song.nil?
