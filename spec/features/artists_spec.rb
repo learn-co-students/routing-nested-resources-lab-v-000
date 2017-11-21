cd182596960e1e7f4a503d86e7936748a1a987eb
charlie  lee <ctlee9096@gmail.com>
2017-11-21 02:57:20 +0000

    Done.

--- spec/features/artists_spec.rb
+++ spec/features/artists_spec.rb
@@ -19,6 +19,7 @@ describe "artists", type: :feature do
 
   describe "GET /artists/id" do
     it "links to artists song path" do
+      #binding.pry
       visit artist_path(@artist)
       expect(page).to have_link(@song.title, href: artist_song_path(@artist, @song))
     end
