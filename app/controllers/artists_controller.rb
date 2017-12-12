3c59288b3b9c962c2c50da2fcfc6c3d5598d7ef9
Amberly <armstrong.amberly@gmail.com>
2017-12-12 10:13:46 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,5 @@
 class ArtistsController < ApplicationController
+  
   def index
     @artists = Artist.all
   end
@@ -13,7 +14,6 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.new(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
