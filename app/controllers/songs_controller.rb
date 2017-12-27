1f9f6fa7d66ac0a1b932f3fc624d51098209be59
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2017-12-27 21:37:15 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,31 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])   
+      if @artist == nil
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      else
+        @songs = @artist.songs
+      end
+    else
+      @songs = Song.all
+    end
   end
 
+
+
   def show
-    @song = Song.find(params[:id])
+    @song = Song.find_by(id: params[:id])
+    if @song == nil
+          flash[:alert] = "Song not found."
+          if params[:artist_id]
+            @artist = Artist.find_by(id: params[:artist_id])
+            redirect_to artist_songs_path(@artist)
+          else
+            redirect_to songs_path  
+          end
+    end
   end
 
   def new
