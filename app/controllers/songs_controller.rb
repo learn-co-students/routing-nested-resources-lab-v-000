669ec8ff89b97f3194a0e8eeaa73c42c97192400
Richard Steinbach <steinbach.dick@gmail.com>
2018-02-16 13:42:35 -0800

    routing-nested-resources-lab-v-000

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,31 @@
+require 'pry'
 class SongsController < ApplicationController
+
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      if Artist.pluck(:id).include?(params[:artist_id].to_i)
+        @songs = Artist.find(params[:artist_id]).songs
+      else
+        flash[:alert] = "Artist not found."
+        redirect_to "/artists"
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    #binding.pry
+    if params[:artist_id]
+      if Song.pluck(:id).include?(params[:id].to_i)
+        @song = Song.find(params[:id])
+      else
+        flash[:alert] = "Song not found."
+        redirect_to "/artists/#{params[:artist_id]}/songs" and return
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
@@ -50,4 +71,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,8 @@
+require 'pry'
 module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  
 end
