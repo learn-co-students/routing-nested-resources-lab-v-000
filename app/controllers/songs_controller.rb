78d45a0e40245ec960c8839e4564071d86fa103d
Alex <github email address>
2018-03-12 14:57:11 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,8 @@
 class SongsController < ApplicationController
   def index  #updated with our nested resources to allow relationship linking
+#binding.pry
     if params[:artist_id]
-        @artist = Artist.find_by(id: params[artist_id])
+        @artist = Artist.find_by(id: params[:artist_id])
         if @artist.nil?
             redirect_to artists_path, alert: "Artist not found"
         else
@@ -17,7 +18,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
         @artist = Artist.find_by(id: params[:artist_id])
         @song = @artist.songs.find_by(id: params[:id])
-        if song.nil?
+        if @song.nil?
           redirect_to artist_songs_path(@artist), alert: "Song not found"
         end
     else
