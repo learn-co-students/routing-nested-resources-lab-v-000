45a5021874e4fc885bfa5a44ba4009b0967d40c5
Alex <github email address>
2018-03-12 14:42:32 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,15 @@ class SongsController < ApplicationController
   end
 
   def show  #does not need to be updated with our nested resources to allow relationship linking to work
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+        @artist = Artist.find_by(id: params[:artist_id])
+        @song = @artist.songs.find_by(id: params[:id])
+        if song.nil?
+          redirect_to artist_songs_path(@artist), alert: "Song not found"
+        end
+    else
+        @song = Song.find(params[:id])
+    end
   end
 
   def new
