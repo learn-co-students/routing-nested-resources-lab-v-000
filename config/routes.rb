b20b5552dac42e3972de90c7cdf2084a32605bdc
github username <github email address>
2018-03-27 22:44:01 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:show] do
-    resources :songs, only: [:show, :index]
+  resources :artists do
+    resources :songs, only: [:index, :show]
   end
-
   resources :songs
-
-  root 'artists#index'
 end
