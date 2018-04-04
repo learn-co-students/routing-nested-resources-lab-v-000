596041db01b1dee74ca43bba52818a916077676b
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-04-04 20:38:11 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,8 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
-        redirect_to artists_path
+        # render :'artists/index'
+         redirect_to artists_path
         flash[:alert] = "Artist not found"
       else
         @songs = @artist.songs
