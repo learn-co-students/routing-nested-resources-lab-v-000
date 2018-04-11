e7fd9e4f1fe50cfed029cd4c998d167911d90e2e
Eva Navarror <emanao@me.com>
2018-04-11 18:25:24 +0000

    GET index spec pass

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,11 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    @artist = Artist.find_by(id: params[:artist_id])
+    if params[:artist_id]
+      @artist? (@songs = @artist.songs) : (redirect_to artists_path)
+    else
+      @songs = Song.all
+    end
   end
 
   def show
@@ -50,4 +55,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  resources :artists, only: [:index, :show] do
+    resources :songs, only: [:index]
+  end
+  resources :songs, only: [:index]
+  #resources :songs, only: [:index, only: [:show, :edit, :update, :destroy]
 end
