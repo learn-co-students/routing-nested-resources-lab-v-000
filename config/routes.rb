3a965979bcfed32d89cf7826e82cf0dda774f11b
smithwebtek <brad@smithwebtek.com>
2018-05-07 16:33:06 -0400

    Update my lab

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:show, :index]
   end
-
-  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
- 
+	resources :songs
 end
