c6d71fdea4091afd1f8d5b4fa9f4dc8f8ff4efb1
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-25 18:34:09 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :artists
+
   resources :songs
+
+  resources :artists do
+    #nested resources for songs
+    resources :songs, only: [:show, :index]
+  end
+
+
+
+
 end
