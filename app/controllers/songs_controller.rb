8c036675ed334c3f08aa5e2412d220e82c82a58e
github username <github email address>
2018-06-10 13:07:52 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
-        redirect_to artists_path, alert: "Artist Not Found"
+        redirect_to artist_path, alert: "Artist Not Found"
         #alert is part of flash
       #good way to do it
       else
