de3e97185e6bd8bb9f56c0a315c6d1bef3341560
TrevorSeitz <trevinla@gmail.com>
2018-07-08 20:47:09 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  # resources :artists
+  # resources :songs
+
+  # get 'artists/:id/songs', to: 'artists#songs_index'
+  resources :artists, only: [:show, :index] do
+    resources :songs, only: [:show, :index]
+  end
+
+  # get 'artists/:id/songs/:song_id' to: 'artists#post'
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+
+  root 'songs#index'
+
 end
