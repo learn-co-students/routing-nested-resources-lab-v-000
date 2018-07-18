46a801b994c89158bbe996cc5b702be3595aec4b
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-18 20:13:15 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
+      binding.pry
       if Artist.find_by(id: params[:artist_id])
         @songs = Artist.find(params[:artist_id]).songs
       else
