5afff4952eb61a0592fdd8064251da9762a2ed2a
Aly Alidina <aly_alidina@hotmail.com>
2018-08-04 13:15:47 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,8 @@
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
-    render template: 'songs/index'
   end
-  
+
   def show
     @artist = Artist.find(params[:id])
   end
