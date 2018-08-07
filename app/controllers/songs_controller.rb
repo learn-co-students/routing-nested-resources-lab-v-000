2946c43ead2e69b9bb440c7cc21422adfe193115
Elaine Anderson <eparie@bu.edu>
2018-08-07 22:34:24 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,15 @@
 class SongsController < ApplicationController
   def index
     @songs = Song.all
+  #  if !song.artist
+   #redirect_to artists_path
   end
 
   def show
     @song = Song.find(params[:id])
+    if !@song.artist
+      #redirect_to artist_songs_path
+    end
   end
 
   def new
@@ -50,4 +55,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
