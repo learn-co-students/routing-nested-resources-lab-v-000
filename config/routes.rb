0a4b55c1680b7c5d53644e5b555c9f427403df9c
rfenix3 <richard.fenix@gmail.com>
2018-09-08 01:47:52 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists, only: [:index, :new, :create, :edit, :update]
+  resources :artists, only: [:show] do
+    resources :songs, only: [:show, :index]
+  end
+
   resources :songs
 end
