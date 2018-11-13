8c8b42fc8d0c97d32548bd142748f22c26d4ebeb
btmccollum <btmcco8@gmail.com>
2018-11-13 11:41:06 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,4 +9,8 @@ class Song < ActiveRecord::Base
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
   end
+
+  def self.valid_artist?(params)
+    Artist.find(params[:artist_id])
+end
 end
