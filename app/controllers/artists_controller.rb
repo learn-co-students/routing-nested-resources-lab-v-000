491a3038e8d35fcab5896656c4b785c648e017cd
harleyharl <harleymteden@gmail.com>
2019-02-18 15:04:53 -0800

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,12 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
+    if Artist.find_by(id: params[:id])
+      @artist = Artist.find_by(id: params[:id])
+    else
+      flash[:alert] = "Artist not found"
+      redirect_to artists_path
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :artists
   resources :songs
+
+  resources :artists, only: [:show] do
+    # nested resource for artists
+    resources :songs, only: [:show, :index]
+  end
 end
