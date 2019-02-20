dfdbbf234da0138775f7914123fe298d01b0891e
Daniel Goldberg <danpaulgo@aol.com>
2019-02-20 07:55:53 +0000

    Complete controller actions for nested routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,27 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      if @artist = Artist.find_by(id: params[:artist_id])
+        @songs = @artist.songs
+      else
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if !params[:artist_id].nil? & Song.find_by(id: params[:id]).nil?
+      if @artist = Artist.find_by(id: params[:artist_id])
+        flash[:alert] = "Song not found"
+        redirect_to artist_songs_path(@artist)
+      end
+    else
+      @song = Song.find(params[:id])
+    end
+    # @song = Song.find(params[:id])
   end
 
   def new
@@ -50,4 +67,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
