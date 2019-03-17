5f5266b17d8d26b32e32e3e975dbf7590db6cdfd
RonSala <ron.sala@gmail.com>
2019-03-17 18:21:27 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :artists
   resources :songs
+
+  resources :artists, only: [:show] do
+    resources :songs, only: [:show, :index]
+  end
+ 
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
