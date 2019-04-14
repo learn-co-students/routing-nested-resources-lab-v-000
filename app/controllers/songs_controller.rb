bc8c217d53a9a933024157d5e4356ecb4716f739
Leslie Stevens <flymaven@protonmail.com>
2019-04-14 14:22:59 -0500

    edit routes, views

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,14 +13,18 @@ class SongsController < ApplicationController
   end
 
   def show
+    #binding.pry
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       @song = @artist.songs.find_by(id: params[:id])
+
       if @song.nil?
         redirect_to artist_songs_path(@artist), alert: "Song not found"
       else
         @song
       end
+    else
+      @song = Song.find(params[:id])
     end
   end
 
