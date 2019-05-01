f61772dd59ce1753a319f5952b5b8e9a0fecd692
Grace Choi <oiograceoio@gmail.com>
2019-05-01 16:08:06 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,12 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
+      @artist = Artist.find(params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path 
+      else
+        @songs = Artist.find(params[:artist_id]).songs
+      end
     else
       @songs = Song.all
     end
