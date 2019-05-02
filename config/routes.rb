f4638b3f311edc9f92d3133f845badf4e58b11ce
github username <github email address>
2019-05-02 14:02:43 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :artists
   resources :songs
+
+  resources :authors, only: [:show] do
+  # nested resource for posts
+  resources :songs, only: [:show, :index]
+end
+
 end
